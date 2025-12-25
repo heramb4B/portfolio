a76d6a17859cfedc7f43cf51785fe561206da1ec
Heramb <heramb.ratech@gmail.com>
2025-12-25 11:34:43 +0530

    major changes

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById("backToTop");
+
+window.onscroll = () => {
+  btn.style.display = window.scrollY > 300 ? "block" : "none";
+};
+
+btn.onclick = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
